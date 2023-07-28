f7d1026a1a2031d40a78f93b7237b39ca2c25910
eason.lee[李宜丞] <eason.lee@104.com.tw>
2023-07-28 18:22:25 +0800

    改為不載入介面

--- popup.js
+++ popup.js
@@ -1,7 +1,7 @@
 chrome.browserAction.onClicked.addListener((tab) => {
     chrome.tabs.getSelected(null, function(tab) {
         let url = tab.url;
-        if(url.match(/raw\.githubusercontent\.com/gi) !== null) {
+        if(url.match(/^https?:\/\/raw\.githubusercontent\.com/gi) !== null) {
             chrome.tabs.update({
                 url: "https://petstore.swagger.io/?url=" + encodeURIComponent(tab.url)
             });
