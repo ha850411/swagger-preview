9b7709a73b83ddd646cf2e88d10520235e56e918
eason.lee[李宜丞] <eason.lee@104.com.tw>
2023-08-08 10:28:29 +0800

    自動監聽網址並轉址

--- popup.js
+++ popup.js
@@ -1,11 +1,23 @@
-chrome.browserAction.onClicked.addListener((tab) => {
-    chrome.tabs.getSelected(null, function(tab) {
-        let url = tab.url;
-        if(url.match(/^https?:\/\/raw\.githubusercontent\.com/gi) !== null) {
-            chrome.tabs.update({
-                url: "https://petstore.swagger.io/?url=" + encodeURIComponent(tab.url)
-            });
-        }
-    });
-});
+// 自動監聽 main_frame 的網址並轉址
+chrome.webRequest.onSendHeaders.addListener(function(detail){
+    if(detail?.type == "main_frame" && detail.url.match(/^https?:\/\/raw\.githubusercontent\.com.*\.(yaml|json)/gi) !== null) {
+        chrome.tabs.update( detail.tabId,{
+            url: "https://petstore.swagger.io/?url=" + encodeURIComponent(detail.url)
+        });
+    }
+},
+{urls: ['<all_urls>']},
+['requestHeaders']);
+
+// 點擊該擴充功能 icon 事件
+// chrome.browserAction.onClicked.addListener((tab) => {
+//     chrome.tabs.getSelected(null, function(tab) {
+//         let url = tab.url;
+//         if(url.match(/^https?:\/\/raw\.githubusercontent\.com/gi) !== null) {
+//             chrome.tabs.update({
+//                 url: "https://petstore.swagger.io/?url=" + encodeURIComponent(tab.url)
+//             });
+//         }
+//     });
+// });
 
